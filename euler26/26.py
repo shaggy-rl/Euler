47122263598a283fc50c353272ee070c12e945da
shaggy-rl <shagla@gmail.com>
2013-11-21 20:57:37 -0800

    solution to project euler problem 26

---
+++ euler26/26.py
@@ -0,0 +1,23 @@
+
+def ReciprocalCycle(n):
+    counter = 1
+    remainder = 10 % n
+
+    while remainder != 1:
+        remainder = remainder * 10 % n
+        counter += 1
+    return counter
+
+def LargestCycle(limit):
+    maxLength = 0
+
+    for n in range(limit, 3, -1):
+        if n % 2 != 0 and n % 5 != 0:
+            x = ReciprocalCycle(n);
+            if x > maxLength:
+                maxNumber = n
+                maxLength = x
+    print maxNumber
+
+LargestCycle(1000)
+#ReciprocalCycle(601)
